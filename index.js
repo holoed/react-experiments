ea6246f33b95d83407c7fd489f5d6a481762ce26
holoed <edmondo@gmail.com>
2015-05-11 00:54:11 +0100

    Playing around with React

---
+++ index.js
@@ -0,0 +1,26 @@
+var Box = React.createClass({
+  getInitialState: function() {
+    return { number: 0 };
+  },
+
+  increaseNumber: function(event) {
+    this.setState({ number: this.state.number + 1 });
+  },
+
+  decreaseNumber: function(event) {
+  	this.setState({ number: this.state.number - 1 });
+  },
+
+  render: function() {
+    return <div>
+               <p>{this.state.number}</p>
+               <button onClick={this.increaseNumber}>Increase</button>
+               <button onClick={this.decreaseNumber}>Decrease</button>
+           </div>
+  }
+});
+
+React.render(
+  React.createElement(Box, null),
+  document.getElementById('content')
+);
